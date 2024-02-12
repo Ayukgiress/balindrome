ac13858a82da7d2e43e200710a850d2c7a77b1e3
AyukGiress <giress865@gmail.com>
2024-02-12 11:18:36 +0100

    editing erros

--- asset/styles/bali.js
+++ asset/styles/bali.js
@@ -1,29 +1,30 @@
 // first call the and give it a function and give it two  string
 // and if the first string is not egaul to second return false else true
 
-function Palindrome(str) {
+function Palindrome (str) {
   let j = str.length - 1;
   for (let i = 0; i < j / 2; i++) {
     if (str[i] !== str[j]) {
-      return false;
+      return false
     }
-    j--;
+    j--
   }
-  return true;
+  return true
 }
 
-console.log(Palindrome("baab"));
+console.log(Palindrome('baab'))
 
 // we then define the function by saying
 //its should check if its a palinndrome or not
-function checkPalindrome() {
-  const word = document.getElementById("placeholder").value;
+
+function checkPalindrome () {
+  const word = document.getElementById('placeholder').value
   const palindrom = Palindrome(word);
-  const answer = document.getElementById("result");
+  const answer = document.getElementById('result')
 
   if (palindrom) {
-    answer.innerHTML = "the word enter is a palindrom";
+    answer.innerHTML = 'the word enter is a palindrom'
   } else {
-    answer.innerHTML = "the word is not a palindrom";
+    answer.innerHTML = 'the word is not a palindrom'
   }
 }
